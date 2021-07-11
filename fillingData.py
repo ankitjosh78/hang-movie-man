aee22a344421473f31e1ab906f42b0053f5f3022
Ankit Josh <ankitjosh78@protonmail.com>
2021-07-11 10:51:59 +0530

    Clear Documentation

--- fillingData.py
+++ fillingData.py
@@ -1,41 +1,58 @@
+""" importing required modules """
+
 from imdb import IMDb
 from imdb import IMDbDataAccessError
 from random import choice
 import sqlite3
 from string import digits
 
-counter=int(input("Enter how many new movies you want?(e.g 10)"))
-count=0
+""" Asking the user the number of movies they want to add """
 
-movieman=IMDb()
-db=sqlite3.connect("movieman.db")
-mc=db.cursor()
+counter = int(input("Enter how many new movies you want?(e.g 10)"))
+count = 0
 
+
+""" Initialising the IMDB class and connecting to the local database """
+
+movieman = IMDb()
+db = sqlite3.connect("movieman.db")
+mc = db.cursor()
+
+""" Checks for no of movies added and adding new movies """
 while counter > 0:
 
-    # generating sequence
-    sequence="00"
+    # generating a sequence
+    sequence = "00"
     for _ in range(5):
-        sequence+=choice(digits)
+        sequence += choice(digits)
 
+    # Trying to get a movie with a uniqueID equal to our generated sequence
     try:
-        movie=movieman.get_movie(sequence)
+
+        movie = movieman.get_movie(sequence)
 
         try:
-            genres=movie['genres']
-            movieID=int(sequence)
-            mc.execute(f"select count(distinct movieID) from movies where movieID={movieID}")
-            answer=mc.fetchall()
 
+            # Getting the genres of the movie
+            genres = movie['genres']
+            movieID = int(sequence)
+
+            # Checking if there already a exists a movie with the same id
+            mc.execute(f"select count(distinct movieID) from movies where movieID={movieID}")
+            answer = mc.fetchall()
+
+            # If there are no duplicates then proceeding
             if answer[0][0] == 0:
-                flag=0
-                movieName=str(movie)
-                movieSummary=str(movie.summary())
+
+                flag = 0
+                movieName = str(movie)
+                movieSummary = str(movie.summary())
 
                 for genre in genres:
-                    sqlQuery="insert into movies values ( ?, ?, ?,?)"
-
+                    sqlQuery = "insert into movies values ( ?, ?, ?,?)"
                     try:
+
+                        # Inserting values into the movies table in the database
                         mc.execute(sqlQuery,(movieID,movieName,genre,movieSummary))
                         db.commit()
                         flag=1
@@ -44,15 +61,23 @@ while counter > 0:
                         pass
                     
                 if flag == 1:
+                    
+                    # Decreasing the counter variable 
                     counter-=1 
+
+                    #Count variable used to show the total movies added so far
                     count+=1
                     print(count,"new movies added to the database.")
 
         except KeyError as _:
+
             pass
         except IMDbDataAccessError as _:
+
             pass
     except IMDbDataAccessError as e:
+
         pass
+
 # Closing the connection when done
 db.close()
--- main.py
+++ main.py
@@ -1,154 +1,207 @@
+""" importing required modules """
+
 from random import choice
 import sqlite3
 
-db=sqlite3.connect("movieman.db")
+""" making connection to the local database """
 
-mc=db.cursor()
+db  =  sqlite3.connect("movieman.db")
 
+mc = db.cursor()
+
+# fetching all the distinct genres from the database
 mc.execute("select distinct genre from movies order by genre asc")
 
-genres=mc.fetchall()
-newGen=[]
+genres = mc.fetchall()
+newGen = []
+
+# storing the genres in the newGen variable
 for x in genres:
     newGen.append(x[0])
 
 
+""" Displaying the user a choice before starting the game """
+
 print("1. Completely random movie.")
 print("2. Movie from specific genres like (Action, Fantasy)")
+
 try:
-    userChoice=int(input("How do you want to guess the movie?"))
+
+    # storing user's initial choice
+    userChoice = int(input("How do you want to guess the movie?"))
 
 except ValueError as _:
+
     print("Please enter a correct number and try again.")
     quit()
 
-if userChoice == 2:
+# If user wants movies from specific genres 
+
+if userChoice  ==  2:
 
     print("Select any genre that you want:")
-    count=1
+    count = 1
+
     for genre in newGen:
         print(count,":",genre)
-        count+=1
+        count += 1
 
     try:
-        genreChoice=int(input("Enter your choice:"))
+
+        # storing user's genre choice
+        genreChoice = int(input("Enter your choice:"))
+
+        # doing some checks
         if genreChoice > count or genreChoice < 1:
+
             print("Please enter a proper number from the given list and try again")
             quit()
+
     except ValueError as _:
+
         print("Please enter a number from the given list and try again.")
         quit()
-    genreMap={}
-    count=1
-    for genre in newGen:
-        genreMap[count]=genre
-        count+=1
 
-    targetGenre=genreMap[genreChoice]
+    # a dictionary to store genre names with their respective code number
+    genreMap = {}
+    count = 1
+    for genre in newGen:
+        genreMap[count] = genre
+        count += 1
+
+    targetGenre = genreMap[genreChoice]
 
     mc.execute("select movieID from movies where genre= ? order by movieID asc",(targetGenre,))
 
-    movieIDs=[]
+    # a list of all the movieIDs from that specific genre
+    movieIDs = []
     for x in mc.fetchall():
         movieIDs.append(x[0])
+
+# If user wants movies to be completely random
 
 elif userChoice == 1:
+
     mc.execute("select distinct movieID from movies order by movieID asc")
-    movieIDs=[]
+
+    # a list of all the movieIDs from that specific genre
+    movieIDs = []
     for x in mc.fetchall():
         movieIDs.append(x[0])
 
-else:
-    print("Please select correct option and try again")
-    quit()
-movieID=choice(movieIDs)
+# If wrong choice
 
+else:
+
+    print("Please select correct option and try again.")
+    quit()
+
+""" Selecting a random movie from the list and getting all details about it """
+
+movieID = choice(movieIDs)
+
+# Storing movieName
 mc.execute("select movieName from movies where movieID=?",(movieID,))
 
 for x in mc.fetchall():
-    movieName=x[0]
+    movieName = x[0]
 
+# Storing movieSummary
 mc.execute("select movieSummary from movies where movieID=?",(movieID,))
 for x in mc.fetchall():
-    movieSummary=x[0]
+    movieSummary = x[0]
 
 # Closing the connnection
 db.close()
 
-actualMovie=movieName
+# Storing the original movie name in a variable
+actualMovie = movieName
 
 # Movie name in string and it's length
-movieName=movieName.lower()
-movieLen=len(movieName) 
+movieName = movieName.lower()
+movieLen = len(movieName) 
 
 # Movie Link
-movieLink="https://www.imdb.com/title/tt"
-movieLink+="00"+str(movieID)
+movieLink = "https://www.imdb.com/title/tt00"
+movieLink += str(movieID)
 
-""" Using a hashmap to store all characters of the movie """
-hashMap={}
+""" Implementation of the actual game starts from here """
+
+# Using a hashmap to store all characters of the movie
+hashMap = {}
 for x in movieName:
-    hashMap[x]=False
+    hashMap[x] = False
 
-# Print dialog
+# Printing hints about the movie title
 print("Okay so your movie is now ready, it is",movieLen,"characters long with",movieName.count(" "),"spaces, ...you have 8 tries to guess the movie.")
+
 # Asking the user for choice
-choice= input("Do you want to start guessing? (y/n)") 
+choice =  input("Do you want to start guessing? (y/n)") 
 
 # If choice is "no"
-if choice.lower() =="n":
+if choice.lower()  == "n":
+
     print("FUCK YOU FOR WASTING MY TIME")
 
 # If choice is not "no"
 else:
 
     # No of tries.
-    tries=8
+    tries = 8
 
     # Checking if all the tries are used.
     while tries > 0 :
 
-        """ Using a flag to check for values in our hashMap """
-        flag=0
+        # Using a flag to check for values in our hashMap
+        flag = 0
+
         for x in hashMap:
-            if hashMap[x]==False:
+
+            if hashMap[x] == False:
                 flag=1
-        if flag==0:
+
+        if flag == 0:
+
             print()
             print("Congratulations, you have succesfully guessed the movie:",actualMovie)
             break
 
-        """ Guessing the letter and doing checks """
-        character_guessed=input("Enter the next letter of the movie:").lower()
+        # Guessing the letter and doing checks
+        character_guessed = input("Enter the next letter of the movie:").lower()
 
         # If guess was correct
-        if character_guessed in movieName and len(character_guessed)==1:
+        if character_guessed in movieName and len(character_guessed) == 1:
 
             # If already guessed this
-            if hashMap[character_guessed]==True:
+            if hashMap[character_guessed] == True:
+
                 print("You have already guessed this.")
                 continue
 
             # If not guessed before
             else:
-                hashMap[character_guessed]=True
+
+                hashMap[character_guessed] = True
                 for c in movieName:
-                    if hashMap[c]==True:
-                        print(c,sep='',end='')
+                    if hashMap[c] == True:
+                        print(c,sep = '',end = '')
                     else:
-                        print('_',sep='',end='')
+                        print('_',sep = '',end = '')
                 print()
 
         # If guess is incorrect
         else:
+
             print("Your guess was incorrect, you have",tries-1,"tries left.")
-            tries-=1
+            tries -= 1
 
     # If all the tries are over.
-    if flag==1:
+    if flag == 1:
+
         print()
         print("You have lost the game...the movie was:", actualMovie)
         print()
+
     # Printing a short summary about the movie
     print("Here's a short summary of the movie:")
     print()
